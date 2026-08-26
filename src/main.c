30478d15bc4cc1ec227341f688f3f8c91f744fdf
simhadhar <simhadharj@gmail.com>
2026-08-26 11:50:05 +0000

    Milestone1

--- src/main.c
+++ src/main.c
@@ -1,84 +1,62 @@
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <readline/history.h>
+
 #include <readline/readline.h>
+#include <readline/history.h>
+
 #include "history.h"
-#include "token.h"
-#include "lexer.h"
-#include "parser.h"
-#include "expand.h"
-#include "builtin.h"
-#include "executor.h"
 
 int main(void)
 {
-    // Display a welcome banner when the shell starts
     printf("=====================================\n");
-    printf("      Shellforge \n");
-    printf(" A Unix Style Shell written in C\n");
+    printf("          Shellforge\n");
+    printf("     A Unix Style Shell in C\n");
     printf("=====================================\n");
 
- token_list_t tokens;
- pipeline_t pipeline;
- 
- char *line;
+    using_history();
+
+    char *line;
 
     while (1)
     {
         line = readline("shellforge$ ");
+
         if (line == NULL)
         {
             printf("\nGoodbye!\n");
             break;
         }
+
         if (strlen(line) == 0)
         {
             free(line);
             continue;
         }
 
-       if (strcmp(line, "history") == 0)
-       {
-          print_history();
-          free(line);
-           continue;
-       }
-// milestone 1 - enabling history
-
-        add_history(line);
-
-// milestone 2.1 - tokenization and lexer
-
-	lexer(line, &tokens);
-
-        // token_print(&tokens);
-
-// milestone 2.2 - expansion of environment variables and parser
-
-	if(parser(&tokens, &pipeline))
-	{
-		expand_variables(&pipeline);
-    	//	pipeline_print(&pipeline);
-	}
-
-
-	for (int i = 0; i < pipeline.command_count; i++)
-	{
-    		int result =
-        	execute_command(&pipeline.commands[i]);
-
-        	if (result == 1)
-    	        {
-        	 free(line);
-		  return 0;
-                  
-        	}
-
+        /* Display command history */
+        if (strcmp(line, "history") == 0)
+        {
+            print_history();
+            free(line);
+            continue;
         }
 
-       free(line);
+        /* Add command to history */
+        add_history(line);
 
+        printf("YOU ENTERED : %s\n", line);
+
+        /* Exit shell */
+        if (strcmp(line, "exit") == 0)
+        {
+            free(line);
+            printf("Exiting...\n");
+            break;
+        }
+
+        free(line);
     }
+
     return 0;
 }
