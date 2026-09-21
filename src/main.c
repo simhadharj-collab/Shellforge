7ffc6119a49bf82b1c0c20ffd15afb3942e02942
simhadhar <simhadharj@gmail.com>
2026-09-21 10:56:38 +0000

    MileStone 4

--- src/main.c
+++ src/main.c
@@ -1,62 +1,86 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 #include <string.h>
-
-#include <readline/readline.h>
 #include <readline/history.h>
-
+#include <readline/readline.h>
 #include "history.h"
+#include "token.h"
+#include "lexer.h"
+#include "parser.h"
+#include "expand.h"
+#include "builtin.h"
+#include "executor.h"
 
 int main(void)
 {
+    // Display a welcome banner when the shell starts
     printf("=====================================\n");
-    printf("          Shellforge\n");
-    printf("     A Unix Style Shell in C\n");
+    printf("      Shellforge \n");
+    printf(" A Unix Style Shell written in C\n");
     printf("=====================================\n");
 
-    using_history();
+ /* =============================================
+       INSTALL BACKGROUND PROCESS HANDLER
+       ============================================= */
 
-    char *line;
+    setup_background_handler();
+
+
+ using_history();
+ token_list_t tokens;
+ pipeline_t pipeline;
+ 
+ char *line;
 
     while (1)
     {
         line = readline("shellforge$ ");
-
         if (line == NULL)
         {
             printf("\nGoodbye!\n");
             break;
         }
-
         if (strlen(line) == 0)
         {
             free(line);
             continue;
         }
 
-        /* Display command history */
-        if (strcmp(line, "history") == 0)
-        {
-            print_history();
-            free(line);
-            continue;
-        }
+       if (strcmp(line, "history") == 0)
+       {
+          print_history();
+          free(line);
+           continue;
+       }
+// milestone 1 - enabling history
 
-        /* Add command to history */
         add_history(line);
 
-        printf("YOU ENTERED : %s\n", line);
+// milestone 2.1 - tokenization and lexer
 
-        /* Exit shell */
-        if (strcmp(line, "exit") == 0)
-        {
-            free(line);
-            printf("Exiting...\n");
-            break;
-        }
+	lexer(line, &tokens);
 
-        free(line);
+        // token_print(&tokens);
+
+// milestone 2.2 - expansion of environment variables and parser
+
+	if(parser(&tokens, &pipeline))
+	{
+		expand_variables(&pipeline);
+    	//	pipeline_print(&pipeline);
+	}
+
+
+	if (pipeline.command_count == 1 &&  pipeline.commands[0].argc > 0 && strcmp(pipeline.commands[0].argv[0],"exit") == 0)
+         {
+                free(line);
+                break;
+            }
+
+        execute_pipeline(&pipeline);
+
+       free(line);
+
     }
-
     return 0;
 }
